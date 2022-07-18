ffbdd0077c67ac7e7610e09886565c452bc5c71e
Hyunho Richard Lee <hrichardlee@gmail.com>
2022-07-18 15:08:09 -0400

    Fixing setup.py, guided_diffusion is a package not a module

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
 
 setup(
-    name="guided-diffusion",
-    py_modules=["guided_diffusion"],
+    name="glid-3-xl",
+    packages=["guided_diffusion", "encoders", "clip_custom"],
     install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+    package_data={"": ["*.gz"]},
 )
